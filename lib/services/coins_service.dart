7e67bab0a35b56e91c2a478e9f98d4db0c009386
BichoyEmad <bichoy.emad@gmail.com>
2025-02-23 01:30:11 +0200

    Fix Coins

--- lib/services/coins_service.dart
+++ lib/services/coins_service.dart
@@ -6,8 +6,7 @@ class CoinsService {
 
   static Future<int> getCoins() async {
     final response = await Request.getFrom('/coins');
-
-    return jsonDecode(response.body)["coins"] ?? 0;
+    return jsonDecode(response.body) ?? 0;
   }
 
 }
