0780e69a53de6882e74bf18c8cb07f6a515b895a
BichoyEmad <bichoy.emad@gmail.com>
2025-11-19 08:02:19 +0200

    feat: add chemistry icon to player cards in lineup

--- lib/widgets/player_card.dart
+++ lib/widgets/player_card.dart
@@ -19,19 +19,46 @@ class PlayerCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onClick,
-      child: SizedBox(
-        width: 900*height/1266,
-        height: height,
-        child: CachedNetworkImage(
-          imageUrl: player.imageUrl ?? '',
-          cacheKey: player.imageKey,
-          width: double.infinity,
-          height: double.infinity,
-          fit: BoxFit.fill,
-          errorWidget: (context, url, error) => errorCardPlaceholder(),
-          placeholder: (context, url) => loadingCardPlaceholder(),
+      child: Stack(children: [
+        SizedBox(
+          width: 900 * height / 1266,
+          height: height,
+          child: CachedNetworkImage(
+            imageUrl: player.imageUrl ?? '',
+            cacheKey: player.imageKey,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.fill,
+            errorWidget: (context, url, error) => errorCardPlaceholder(),
+            placeholder: (context, url) => loadingCardPlaceholder(),
+          ),
         ),
-      ),
+        if (player.chemistry>0 && player.chemistry<4)
+          Positioned(
+            left: 0,
+            bottom: 10,
+            width: 20,
+            height: 20,
+            child: Image.asset(
+              _getChemistryAsset(player.chemistry),
+              fit: BoxFit.contain,
+            ),
+          ),
+      ]),
     );
   }
+
+  String _getChemistryAsset(int chemistry) {
+    switch (chemistry) {
+      case 1:
+        return 'assets/icons/chemistry_1.png';
+      case 2:
+        return 'assets/icons/chemistry_2.png';
+      case 3:
+        return 'assets/icons/chemistry_3.png';
+      default:
+        return '';
+    }
+  }
+
 }
