c5d5a105b14a501915616c06e47a336f278c72dc
BichoyEmad <bichoy.emad@gmail.com>
2025-11-19 07:33:44 +0200

    improve store reloading

--- lib/providers/players_store_provider.dart
+++ lib/providers/players_store_provider.dart
@@ -6,11 +6,14 @@ class PlayersStoreProvider with ChangeNotifier {
 
   List<Player> _items = <Player>[];
   bool _isLoading = false;
+  String _currentPosition = '';
 
   List<Player> get items => _items;
   bool get isLoading => _isLoading;
 
   Future<void> loadStore(String position) async {
+    if (position == _currentPosition) return;
+    _currentPosition = position;
     _isLoading = true;
     _items = <Player>[];
     notifyListeners();
